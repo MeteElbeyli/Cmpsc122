e7a99209bce4c2dcbf781a300d441ed5da859522
Mete Elbeyli <42346128+MeteElbeyli@users.noreply.github.com>
2023-05-02 21:35:48 -0400

    Add files via upload

---
+++ IntegerVectorfor122/Linkedlistex/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Linkedlistex
+//
+//  Created by Mete Elbeyli on 2/22/22.
+//
+
+#include <iostream>
+using namespace std;
+
+
+class SLNode {
+
+private:
+    int data;
+    SLNode *next;
+    
+public:
+    SLNode();
+    
+    SLNode (int data, SLN *next)
+}
+
+
+int main(
+         cout <<"hello";
+         return 0;
+}
+         
